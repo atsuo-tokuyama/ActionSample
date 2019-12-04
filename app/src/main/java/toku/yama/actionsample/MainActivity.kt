8b0f536191ff947ecd07230ebab9489efb640339
Atsuo Tokuyama <atsuo.tokuyama@westunitis.co.jp>
2019-12-04 16:24:41 +0900

    ユニットテストを追加

--- app/src/main/java/toku/yama/actionsample/MainActivity.kt
+++ app/src/main/java/toku/yama/actionsample/MainActivity.kt
@@ -8,5 +8,12 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val ans = addition(1, 3)
+        println(ans)
+    }
+
+    fun addition(a:Int, b:Int):Int {
+        return a+b
     }
 }
---
+++ app/src/test/java/toku/yama/actionsample/MainActivityTest.kt
@@ -0,0 +1,13 @@
+package toku.yama.actionsample
+
+import org.junit.Test
+
+import org.junit.Assert.*
+
+class MainActivityTest {
+
+    @Test
+    fun addition() {
+        assertEquals(4, 2 - 2)
+    }
+}
